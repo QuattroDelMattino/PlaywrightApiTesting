c8a034a72be3deb3aaf5be6dd22969b7a134535d
Sorina Sasu <sorinasassu@gmail.com>
2024-08-06 17:10:24 +0300

    Add tests for withdraw endpoint

--- OnlineWalletTestApi/OnlineWalletApiTests.cs
+++ OnlineWalletTestApi/OnlineWalletApiTests.cs
@@ -40,22 +40,17 @@ public class OnlineWalletApiTests : PlaywrightTest
     [TestMethod]
     public async Task Test_WalletDeposit_OkStatus()
     {
-        var data = new Dictionary<string, double>
-        {
-            { "amount", 3 }
-        };
-        
-        var depositResponse = await Request.PostAsync("/onlinewallet/deposit", new() { DataObject = data });
+        var depositResponse = await DepositWalletAmount(3);
         await Assertions.Expect(depositResponse).ToBeOKAsync();
 
         JsonElement? res = await depositResponse.JsonAsync();
         var amountValue = res?.GetProperty("amount");
         Assert.AreEqual(3, amountValue?.GetDouble()!);
-        Assert.AreEqual(3, await GetAmountValue());
+        Assert.AreEqual(3, await GetBalance());
     }
     
     [TestMethod]
-    public async Task Test_WalletDeposit_String()
+    public async Task Test_WalletDeposit_String_OkStatus()
     {
         var data = new Dictionary<string, string>
         {
@@ -68,7 +63,7 @@ public class OnlineWalletApiTests : PlaywrightTest
         JsonElement? res = await depositResponse.JsonAsync();
         var amountValue = res?.GetProperty("amount");
         Assert.AreEqual(3, amountValue?.GetDouble()!);
-        Assert.AreEqual(3, await GetAmountValue());
+        Assert.AreEqual(3, await GetBalance());
     }
     
     [TestMethod]
@@ -86,9 +81,7 @@ public class OnlineWalletApiTests : PlaywrightTest
         var errors = jsonBody?.GetProperty("errors");
         var depositRequest = errors?.GetProperty("depositRequest").EnumerateArray();
         Assert.AreEqual("The depositRequest field is required.", depositRequest?.First().GetString());
-        
         var amount = errors?.GetProperty("$.amount").EnumerateArray();
-        // Assert.AreEqual("The JSON value could not be converted to System.Decimal", amount?.First().GetString());
         Assert.IsTrue(amount?.First().GetString().Contains("The JSON value could not be converted to System.Decimal"));
     }
     
@@ -126,10 +119,92 @@ public class OnlineWalletApiTests : PlaywrightTest
         Assert.AreEqual(415, depositResponse.Status);
         Assert.AreEqual("Unsupported Media Type", depositResponse.StatusText);
     }
+    
+    [TestMethod]
+    public async Task Test_WalletWithdrawal_OkStatus()
+    {
+        var data = new Dictionary<string, double>
+        {
+            { "amount", 0 }
+        };
+        var withdrawResponse = await Request.PostAsync("/onlinewallet/withdraw", new() { DataObject = data });
 
+        await Assertions.Expect(withdrawResponse).ToBeOKAsync();
+
+        JsonElement? res = await withdrawResponse.JsonAsync();
+        var amountValue = res?.GetProperty("amount");
+        Assert.AreEqual(0, amountValue?.GetDouble()!);
+        Assert.AreEqual(0, await GetBalance());
+    }
+    
+    [TestMethod]
+    public async Task Test_WalletWithdrawal_String_OkStatus()
+    {
+        await DepositWalletAmount(3);
+        var data = new Dictionary<string, string>
+        {
+            { "amount", "3" }
+        };
+        
+        var depositResponse = await Request.PostAsync("/onlinewallet/withdraw", new() { DataObject = data });
+        await Assertions.Expect(depositResponse).ToBeOKAsync();
+
+        JsonElement? res = await depositResponse.JsonAsync();
+        var amountValue = res?.GetProperty("amount");
+        Assert.AreEqual(0, amountValue?.GetDouble()!);
+        Assert.AreEqual(0, await GetBalance());
+    }
+    
+    [TestMethod]
+    public async Task Test_WalletWithdrawal_InsufficientFunds_ExpectBadRequest()
+    {
+        await DepositWalletAmount(33);
+        var withdrawResponse = await WithdrawWalletAmount(55);
+
+        Assert.AreEqual(400, withdrawResponse.Status);
+        Assert.AreEqual("Bad Request", withdrawResponse.StatusText); 
+        
+        JsonElement? jsonBody = await withdrawResponse.JsonAsync();
+        Assert.AreEqual("InsufficientBalanceException", jsonBody?.GetProperty("type").GetString());
+        Assert.AreEqual("Invalid withdrawal amount. There are insufficient funds.", jsonBody?.GetProperty("title").GetString());
+        Assert.IsNotNull( jsonBody?.GetProperty("detail"));
+    }
+    
+    [TestMethod]
+    public async Task Test_WalletWithdrawal_NegativeAmount_ExpectBadRequest()
+    {
+        var data = new Dictionary<string, string>
+        {
+            { "amount", "-5" }
+        };
+        
+        var depositResponse = await Request.PostAsync("/onlinewallet/withdraw", new() { DataObject = data });
+        Assert.AreEqual(400, depositResponse.Status);
+        Assert.AreEqual("Bad Request", depositResponse.StatusText);
+        JsonElement? jsonBody = await depositResponse.JsonAsync();
+        var errors = jsonBody?.GetProperty("errors").GetProperty("Amount").EnumerateArray();
+        Assert.AreEqual("'Amount' must be greater than or equal to '0'.", errors?.First().GetString());
+    }
+
+    [TestMethod]
+    public async Task Test_WalletWithdrawal_EmptyPayload_ExpectBalanceNotChanged()
+    {
+        var data = new Dictionary<string, string>();
+        var depositResponse = await Request.PostAsync("/onlinewallet/withdraw", new() { DataObject = data });
+        await CheckBalanceIsZero(depositResponse);
+    }
+
+    [TestMethod]
+    public async Task Test_WalletWithdrawal_NoPayload_ExpectUnsupportedMediaType()
+    {
+        var depositResponse = await Request.PostAsync("/onlinewallet/withdraw", new() { DataObject = null });
+        Assert.AreEqual(415, depositResponse.Status);
+        Assert.AreEqual("Unsupported Media Type", depositResponse.StatusText);
+    }
+    
     private async Task ResetApiState()
     {
-        var currentBalance = await GetAmountValue();
+        var currentBalance = await GetBalance();
         if (currentBalance > 0)
         {
             var response = await WithdrawWalletAmount(currentBalance);
@@ -160,6 +235,16 @@ public class OnlineWalletApiTests : PlaywrightTest
         var response = await Request.GetAsync("/onlinewallet/balance");
         return response;
     }
+    
+    private async Task<IAPIResponse> DepositWalletAmount(double amount)
+    {
+        var data = new Dictionary<string, double>
+        {
+            { "amount", amount }
+        };
+        
+        return await Request.PostAsync("/onlinewallet/deposit", new() { DataObject = data });
+    }
 
     private async Task<IAPIResponse> WithdrawWalletAmount(double currentBalance)
     {
@@ -170,7 +255,7 @@ public class OnlineWalletApiTests : PlaywrightTest
         return await Request.PostAsync("/onlinewallet/withdraw", new() { DataObject = data });
     }
 
-    private async Task<double> GetAmountValue()
+    private async Task<double> GetBalance()
     {
         var response = await GetWalletBalance();
 
